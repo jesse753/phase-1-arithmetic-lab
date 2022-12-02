e57a3211b2b4ade2a0ac01a057b23d2c52c0eb5b
jesse <jesse.kibaara@student.moringaschool.com>
2022-12-03 00:12:40 +0300

    lab done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const multiply = (num1, num2) => {
+    return num1 * num2;
+    };
+    let resulMultiply = multiply(2, 31);
+    console. log(resulMultiply);
+    // 62.
 
+
+
+// Returns a random number:
+let random=1;Math.random(); 
+let counter=24;counter %= 10;
+//=> 4
+let max=20; Math.max(1,5,10,15,20);
 // Write your code here
